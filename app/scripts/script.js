5f2d9bccf3ea5db1746da92624808e948b2fb739
anna-mish <anna-mish@users.noreply.github.com>
2019-09-21 10:11:31 +0300

    ___

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,5 +1,5 @@
-import './libs/jquery'; 
-import './libs/swiper/swiper'; 
+import $ from './libs/jquery'; 
+import Swiper from 'swiper';
 
 $( document ).ready(function() { 
   var swiper = new Swiper('.swiper-container', {
@@ -8,3 +8,23 @@ $( document ).ready(function() {
     },
   });
 });
+
+// $('.swiper-object .swiper-container').each(function(index, value) {
+ 
+//   var mySwiper = new Swiper(value, {
+//     autoplay: {
+//       delay: 5000,
+//     },
+//     navigation: {
+//       nextEl: value.nextElementSibling.nextElementSibling,
+//       prevEl: value.nextElementSibling,
+//     },
+//     pagination: {
+//       el: value.nextElementSibling.nextElementSibling.nextElementSibling,
+//       clickable: true,
+//     },
+//     slidesPerView: 'auto',
+//   });
+
+// });
+
